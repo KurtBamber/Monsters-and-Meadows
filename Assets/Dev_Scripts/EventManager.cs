2ddb993a18ca79734bf153fb6c283041af2f6efb
ethandoig22 <ethan.doig@sky.com>
2025-03-22 16:48:13 +0000

    Event Manager Added

--- Assets/Dev_Scripts/EventManager.cs
+++ Assets/Dev_Scripts/EventManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Event Properties")]
+    public int randomNum;
+    public int scarinessOfMonsters;
+    public float howLongTillNextEvent;
+    public string[] eventNotifications;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Raid Properties")]
+    public int raidDifficulty;
+
+    [Header("Human Scout Properties")]
+    public int likelyhoodOfScoutReporting;
+
+    [Header("Monster Encounter Properties")]
+    public int likelyhoodOfMonsterEncounter;
+    public int whichMonster;
+    public GameObject[] monsters;
+
+    [Header("Potential Trader Properties")]
+    public int traderLuck;
+    public int whichTrader;
+    public GameObject[] traders;
 }
